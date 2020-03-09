f0552e411e6b4bc7839dbd4427429b692384da19
Keri Levesque <kerianne166@gmail.com>
2020-03-09 16:18:26 -0400

    added starter project

---
+++ Albums/Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Keri Levesque on 3/9/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
